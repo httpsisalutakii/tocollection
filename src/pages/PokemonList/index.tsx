35def51b99e824594c22d6e3d59d86a78f74514f
unknown <isabelliluiza240205@gmail.com>
2026-03-17 22:08:55 -0300

    https://github.com/httpsisalutakii/tocollection.git

--- src/pages/PokemonList/index.tsx
+++ src/pages/PokemonList/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, FlatList, Image, TouchableOpacity, Button } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, FlatList, Image, TouchableOpacity, Button, ActivityIndicator} from 'react-native';
 import { createStyles } from './styles';
 import { useTheme } from '../../global/themes';
 import { useNavigation } from '@react-navigation/native';
@@ -39,6 +39,29 @@ export default function PokemonListScreen() {
   const styles = createStyles(theme);
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList, 'PokemonList'>>();
   
+  const [pokemons, setPokemons] = useState<PokemonListItem[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setError(null);
+
+    const timer = setTimeout(() => {
+      try {
+        setPokemons(MOCK_POKEMON_LIST);
+      } catch (error) {
+        setError('Falha ao carrecar lista de comunidades!');
+      } finally {
+        setIsLoading(false);
+      }
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, []);
+
   const handleLogout = () => {
     navigation.reset({
       index: 0,
@@ -61,6 +84,22 @@ export default function PokemonListScreen() {
       <Image source={{ uri: item.imageUrl }} style={styles.cardImage} />
     </TouchableOpacity>
   );
+
+  if (isLoading) {
+    return (
+      <View style={[styles.container, { justifyContent: 'center', alignItems: 'center' }]}>
+        <ActivityIndicator size="large" color={theme.colors.primary} />
+        <Text style={{ marginTop: 16, color: theme.colors.text }}>Carregando lista (simulado)...</Text>
+      </View>
+    );
+  }
+  if (error) {
+    return (
+      <View style={[styles.container, { justifyContent: 'center', alignItems: 'center' }]}>
+        <Text style={{ color: theme.colors.text, marginBottom: 16 }}>{error}</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
